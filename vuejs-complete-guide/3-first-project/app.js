fc408eff6315449aa5f1bf9f44208a53d85e6342
Gedson Marcelino <gedson.marcelino@gmail.com>
2017-12-18 16:36:28 -0300

    - check if game is running - added log actions

--- vuejs-complete-guide/3-first-project/app.js
+++ vuejs-complete-guide/3-first-project/app.js
@@ -17,15 +17,19 @@ new Vue({
 
         endGame: false,
 
-        log: []
+        gameRunning: false,
+
+        logs: []
     },
 
     methods: {
         startGame: function(){
             console.log('Start game!');
+            this.gameRunning = true;
             this.player.health = 100;
             this.monster.health = 100;
             this.endGame = false;
+            this.logs = [];
         },
         
         attack:function(){
@@ -34,7 +38,10 @@ new Vue({
             var monsterAttack = this.monster.attack[this._getRandomIntInclusive(0, (this.monster.attack.length-1))];
 
             this.player.health -= monsterAttack;
+            this._actionLog( 2, monsterAttack);
+            
             this.monster.health -= playerAttack;
+            this._actionLog( 1, playerAttack);            
         },
 
         specialAttack: function(){
@@ -43,21 +50,47 @@ new Vue({
             var monsterAttack = this.monster.attack[this._getRandomIntInclusive(0, (this.monster.attack.length-1))];
 
             this.player.health -= monsterAttack;
+            this._actionLog( 2, monsterAttack);
+            
             this.monster.health -= playerAttack;
+            this._actionLog( 1, monsterAttack, 'SPECIAL ATTACK');
         },
 
         heal: function(){
             console.log('Heal!');
             var monsterAttack = this.monster.attack[this._getRandomIntInclusive(0, (this.monster.attack.length-1))];
+            var playerHeal = this._getRandomIntInclusive(5, 10);
 
             this.player.health -= monsterAttack;
-            this.player.health += this._getRandomIntInclusive(5, 10);
+            this._actionLog( 2, monsterAttack);
+            
+            this.player.health += playerHeal;
+            this._actionLog( 1, playerHeal, 'HEAL');
         },
 
         _getRandomIntInclusive: function(min, max) {
             min = Math.ceil(min);
             max = Math.floor(max);
             return Math.floor(Math.random() * (max - min + 1)) + min;
+        },
+
+        _actionLog: function( player, damage, type = 'hit' ){
+
+            if ( player == 1 ){
+                player1 = 'YOU';
+                player2 = 'MONSTER';
+            } else {
+                player1 = 'MONSTER';
+                player2 = 'YOU';
+            }
+
+            this.logs.push( player1+' '+type+' '+damage+' in '+player2+'.');
+        }
+    },
+
+    computed: {
+        reverseLogs: function(){
+            return this.logs.reverse();
         }
     },
 
@@ -83,6 +116,8 @@ new Vue({
         endGame:function( newValue, oldValue ){
             var vm = this;
             if ( newValue == true ){
+                this.gameRunning = false;
+                this.endGame = true;
                 setTimeout(function(){
                     if ( vm.monster.health == 0 && vm.player.health == 0 ){
                         alert('DRAW!!');
