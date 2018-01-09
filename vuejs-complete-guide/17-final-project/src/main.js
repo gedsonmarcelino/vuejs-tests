0f79e57b3b18d3a0a580679fe93c3fa3fb967dc6
Gedson Marcelino <gedson.marcelino@gmail.com>
2018-01-09 18:19:23 -0300

    - module final project

--- vuejs-complete-guide/17-final-project/src/main.js
+++ vuejs-complete-guide/17-final-project/src/main.js
@@ -1,7 +1,29 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
+
 import App from './App.vue'
+
+import {routes} from './routes.js'
+import store from './store/store'
+
+Vue.use(VueRouter)
+Vue.use(VueResource)
+
+Vue.http.options.root = 'https://vue-http-test-5bfdd.firebaseio.com/';
+
+Vue.filter('currency', (value) => {
+	return '$' + value.toLocaleString();
+});
+
+const router = new VueRouter({
+  routes,
+  mode: 'history'
+})
 
 new Vue({
   el: '#app',
+  router,
+  store,
   render: h => h(App)
 })
---
+++ vuejs-complete-guide/17-final-project/src/store/store.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+import Stocks from './modules/stocks.js'
+import Portfolio from './modules/portfolio.js'
+
+import * as actions from './actions';
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+    actions,
+    modules: {
+        Stocks,
+        Portfolio
+    }
+})
